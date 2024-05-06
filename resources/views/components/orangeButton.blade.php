b8d2d1142dc07f148aeaa0bc3f2ce3a21f5ae369
Nuno Silva <nunobasket111@gmail.com>
2024-05-06 22:51:53 +0100

    Install Breeze

---
+++ resources/views/components/orangeButton.blade.php
@@ -0,0 +1,2 @@
+<button {{ $attributes->merge(['class'=>'rounded-md ml-5 bg-orange-400 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-200 hover:text-gray-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600']) }}"> {{$slot}}</button>
+
